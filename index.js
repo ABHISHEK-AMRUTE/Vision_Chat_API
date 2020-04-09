801ff566d393912ed01a569f70fed01f9232c91b
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-10 00:06:01 +0530

    RealTime Database initialized

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const express = require('express')
 const main_routes = require('./src/routes/main_routes')
 const app =  express()
-const keys = require('./keys')
-
-var firebaseConfig = {
-    apiKey: keys.firebase,
-    authDomain: "vision-chat-api.firebaseapp.com",
-    databaseURL: "https://vision-chat-api.firebaseio.com",
-    projectId: "vision-chat-api",
-    storageBucket: "vision-chat-api.appspot.com",
-    messagingSenderId: "597352145530",
-    appId: "1:597352145530:web:8ee6099df47160593551c3",
-    measurementId: "G-Y3246H3C18"
-  };
+const database = require('./src/firebase/firebase')
 
 
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(main_routes)
 
+
+
+database.ref('/users').set({
+    username: "name",
+    email: "email",
+    profile_picture : "imageUrl"
+  });
 
 app.listen(port,()=>{
     console.log('Server is up on port: '+port)
