f350a9cce846bff1a3bd7cf7af3edebb3f3fc7e8
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-09 23:48:46 +0530

    Firebse initialised

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const main_routes = require('./src/routes/main_routes')
 const app =  express()
+const keys = require('./keys')
+
+var firebaseConfig = {
+    apiKey: keys.firebase,
+    authDomain: "vision-chat-api.firebaseapp.com",
+    databaseURL: "https://vision-chat-api.firebaseio.com",
+    projectId: "vision-chat-api",
+    storageBucket: "vision-chat-api.appspot.com",
+    messagingSenderId: "597352145530",
+    appId: "1:597352145530:web:8ee6099df47160593551c3",
+    measurementId: "G-Y3246H3C18"
+  };
+
 
 const port = process.env.PORT || 3000
 app.use(express.json())
