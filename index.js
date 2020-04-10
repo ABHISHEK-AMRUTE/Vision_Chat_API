c35afa44dd74110cddafa29d4d9e5608fb4e738d
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-10 11:57:27 +0530

    templets created

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ const express = require('express')
 const main_routes = require('./src/routes/main_routes')
 const app =  express()
 const database = require('./src/firebase/firebase')
-
+const path  =require('path')
+const hbs = require('hbs')
 
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(main_routes)
+var pathdirectory= path.join(__dirname,'./public')
+app.use(express.static(pathdirectory))
+app.set('view engine','hbs')
+var  viewpath =path.join(__dirname,'./templets/views')
+app.set('views',viewpath)
 
-
-
-// database.ref('/users').set({
-//     username: "name",
-//     email: "email",
-//     profile_picture : "imageUrl"
-//   });
 
 app.listen(port,()=>{
     console.log('Server is up on port: '+port)
--- src/routes/main_routes.js
+++ src/routes/main_routes.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = new express.Router()
 const database = require('../firebase/firebase')
 
-app.get('/user',(req,res)=>
-{
-    res.send("form app congrats!")
+//////////landing page of the chat interface/////
+app.get('',(req,res)=>{
+    res.render('chat',{
+        tit : "title of the page"
+    })
 })
-
 
 //////chatting: pushing chat over server
 app.get('/chat/:usrname/:message',(req,res)=>
@@ -22,4 +23,10 @@ app.get('/chat/',(req,res)=>
 {
     res.send("form app congrats!")
 })
+
+///creating room and password
+app.get('/createRoom/')
+
+
+
 module.exports = app
