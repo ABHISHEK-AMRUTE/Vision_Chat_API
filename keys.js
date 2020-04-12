fa2b69980bb6190274d75ad9bc5b88c4bced6186
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-12 11:30:12 +0530

    configuration

---
+++ keys.js
@@ -0,0 +1,11 @@
+const keys = {
+   apiKey: process.env.KEYS,
+    authDomain: "vision-chat-api.firebaseapp.com",
+    databaseURL: "https://vision-chat-api.firebaseio.com",
+    projectId: "vision-chat-api",
+    storageBucket: "vision-chat-api.appspot.com",
+    messagingSenderId: "597352145530",
+    appId: "1:597352145530:web:8ee6099df47160593551c3",
+    measurementId: "G-Y3246H3C18"
+}
+module.exports = keys
