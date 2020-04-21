0942765ce104b53ccac8cecf408880d99572c773
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-21 18:56:08 +0530

    Redundancy removing algorithms

--- src/routes/main_routes.js
+++ src/routes/main_routes.js
@@ -22,19 +22,42 @@ app.get('/chat/:usrname/:message',(req,res)=>{database.ref('/user/'+Date.now()).
 app.get('/createRoom',(req,res)=>{
     const userId  =req.query.userId;
     console.log(userId)
-    database.ref('/'+userId+"/"+Date.now()).set({username:"admin",message:"room created"})
-    //////temp
-    database.ref('/user_count_on_create_room/'+Date.now()+'/').set({date:Date.now()})
-    const url = process.env.URL + '/user_reg?roomID='+req.query.userId
-    console.log(url)
-    res.send({url})
+    database.ref('/').once('value', function(snapshot) {
+        var data = snapshot.val();
+        var flag=0
+        for(let key in data){
+         if(key==userId)
+         {
+           flag=1
+         }
+        }
+        if(flag==0)
+        {
+            database.ref('/'+userId+"/"+Date.now()).set({username:"admin",message:"room created"})
+            //////temp
+            database.ref('/user_count_on_create_room/'+Date.now()+'/').set({date:Date.now()})
+            const url = process.env.URL + '/user_reg?roomID='+req.query.userId
+            console.log(url)
+            res.send({url})
+        }else{
+            console.log('Room already exists')
+            res.send({error:'Be unique'})
+        }
+         
+  
+       
+       
+       
+      });
+
+   
 })
 
 
 //// loading 
 app.get('/user_reg',(req,res)=>{
     console.log(req.query.roomID)
-     
+    
     res.render('user_reg',{userID:req.query.roomID})
 })
 
@@ -42,9 +65,28 @@ app.get('/user_reg',(req,res)=>{
 app.get('/add_user/to_list',(req,res)=>{
     //////temp
     database.ref('/user_count_on_reg_users/'+Date.now()+'/').set({date:Date.now()})
-    database.ref('/'+req.query.roomID+"_info/"+req.query.user_name).set({joinedat:Date.now()})
-    console.log('user_added_to_list')
-    res.send();
+    
+    database.ref('/'+req.query.roomID+"_info/").once('value', function(snapshot) {
+        var data = snapshot.val();
+        var flag=0
+        for(let key in data){
+            console.log(key + "  with  "+req.query.user_name)
+         if(key==req.query.user_name)
+         {
+           flag=1
+         }
+        }
+        if(flag==0)
+        {
+            database.ref('/'+req.query.roomID+"_info/"+req.query.user_name).set({joinedat:Date.now()})
+            console.log('user_added_to_list')
+            res.send({ok:'ok'});
+        }else{
+            console.log('User already exists or person with same name exists.')
+            res.send({error:'User already exists or person with same name exists.'})
+        }
+       });
+    
 })
 
 
