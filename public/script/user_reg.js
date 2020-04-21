ba15ae879f2a8124e1c4e7fd61a2c65c024dfc2b
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-21 18:55:58 +0530

    Redundancy removing algorithms

--- public/script/user_reg.js
+++ public/script/user_reg.js
@@ -1,4 +1,3 @@
-
 function getQueryVariable(variable)
 {
        var query = window.location.search.substring(1);
@@ -12,17 +11,23 @@ function getQueryVariable(variable)
 const userID= getQueryVariable('roomID')
 console.log('user room name '+userID)
 document.getElementById('submit').addEventListener('click',function(){
-
+   document.getElementById('error_message').innerHTML=""
     
     fetch('/add_user/to_list?roomID='+userID+'&user_name='+document.getElementById('input').value).then((res)=>{
         res.json().then((data)=>{
-             
+            if(data.error)
+            {
+               document.getElementById('error_message').innerHTML=data.error
+            }
+            else{
+               window.location.replace('../html/chat.html?userID='+ userID
+               +'&username='+document.getElementById('input').value);
+            }
         }) 
        
      })
 
-    window.location.replace('../html/chat.html?userID='+ userID
-    +'&username='+document.getElementById('input').value);
+    
     
 
 })
