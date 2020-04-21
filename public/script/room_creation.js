f5e391d5d4bf2010131a70fd0accdb10a5765808
Abhishek <abhishekamrutelfs@gmail.com>
2020-04-21 18:55:44 +0530

    Redundancy removing algorithms

--- public/script/room_creation.js
+++ public/script/room_creation.js
@@ -1,6 +1,5 @@
 
-// document.getElementById('submit').addEventListener('click',function(){
-   
+// document.getElementById('submit').addEventListener('click',function  
 // })
 function callme(){
     
@@ -8,16 +7,30 @@ function callme(){
     
     fetch('/createRoom?userId='+str).then((res)=>{
        res.json().then((data)=>{
+         if(data.url){
            document.getElementById('message').value = data.url 
-           
+           }
+           else{
+            name_message()
+           }
        }) 
       
     })
 }
+function name_message() {
+    
+  var x = document.getElementById("snackbar");
+  x.innerHTML="Room name already taken! Try to be unique."
+
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  
+
+}
 function copy_to() {
     
     var x = document.getElementById("snackbar");
-  
+    x.innerHTML="Text copied to clipboard"
   
     x.className = "show";
   
